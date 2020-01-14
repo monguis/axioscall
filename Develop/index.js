da0953604aa0e43ec040f715f2f3cf4e09a3b5c1
monguis <jccnnetwork@gmail.com>
2020-01-14 16:58:17 -0700

    created basic file structure

---
+++ Develop/index.js
@@ -0,0 +1,29 @@
+const generateHTML = require("./generateHTML");
+
+const questions = [
+  
+];
+
+function writeToFile(fileName, data) {
+ 
+}
+
+function init() {
+    const fs = require("fs");
+    const axios = require("axios");
+    const inquirer = require("inquirer");
+
+    inquirer
+        .prompt({
+            message: "Enter your GitHub username",
+            name: "username"
+        })
+        .then(function ({ username }) {
+            const queryUrl = `https://api.github.com/users/${username}/repos?per_page=100`;
+            axios.get(queryUrl).then(function (data) { console.log(data.data[0].name) 
+            generateHTML.pdfGen(data)})
+            
+        });
+    }
+
+init();
